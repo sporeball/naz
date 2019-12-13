11ad9b8ab04df1f54928e4faa055a0c4a56998cd
sporeball <sporeballdev@gmail.com>
2019-12-12 20:34:52 -0800

    setup

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+  index.js
+  naz interpreter
+  copyright (c) 2019 sporeball
+  MIT license
+*/
+
+var ptr = 0;
+
+function parse(code) {
+  console.log(code);
+  return;
+}
+
+exports.parse = parse;
---
+++ naz.js
@@ -0,0 +1,26 @@
+/*
+  naz.js
+  interface to naz interpreter
+  copyright (c) 2019 sporeball
+  MIT license
+*/
+
+const Naz = require("./index.js")
+const fs = require("fs")
+const path = require("path")
+
+function naz() {
+  var args = process.argv.slice(2);
+  var file = args[0];
+  if (file.slice(-4) != ".naz") {
+    console.log("error: not a .naz file");
+    return;
+  }
+  
+  code = fs.readFileSync(path.join(__dirname, file), {encoding: "utf-8"}, function(){});
+  code = code.split(/\r?\n/).map(l => l.split(""));
+  
+  Naz.parse(code);
+}
+
+naz();
